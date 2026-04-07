a3631056749504028dad75ef8e0cd719f9751f86
Semucas <semucas27@gmail.com>
2026-04-07 01:54:28 -0500

    docs: Agregar datos de prueba para demostración

--- estudiantes/admin.py
+++ estudiantes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Estudiante
 
-# Register your models here.
+@admin.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'apellido', 'email', 'programa', 'semestre', 'activo')
+    list_filter = ('activo', 'programa', 'semestre')
+    search_fields = ('codigo', 'nombre', 'apellido', 'email')
+    list_per_page = 20
+    ordering = ('codigo',)
